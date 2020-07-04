81707617622d7345a38b7444aef8ff880a4d0e28
SashoLichovski <licovskiph@gmail.com>
2020-07-04 18:35:50 +0200

    Done

---
+++ CustomerSupport/CustomerSupport/Models/CustomerSupportDbContext.cs
@@ -0,0 +1,21 @@
+﻿using CustomerSupport.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomerSupport.Models
+{
+    public class CustomerSupportDbContext : IdentityDbContext<User>
+    {
+        public CustomerSupportDbContext(DbContextOptions<CustomerSupportDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Ticket> Tickets { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+    }
+}
